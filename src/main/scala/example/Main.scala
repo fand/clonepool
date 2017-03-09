96f087fdb46c81955d6c77335dfdedf9df6fbbea
amagitakayosi <fand.gmork@gmail.com>
2017-03-09 12:12:55 +0900

    Fetch ok

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -2,35 +2,72 @@ package io.github.fand.clonepool
 import java.io.File
 import org.eclipse.jgit.api._
 import org.eclipse.jgit.lib._
+import org.eclipse.jgit.api.errors._
+import scala.collection.JavaConversions._
 
 object Main extends App {
 
-  /**
-   * まずCloneなりローカルなレポジトリを教えるなりして生成したgitのインスタンスを叩くことで各種レポジトリ操作を実行する。
-   * コマンドはメソッドチェインで構築し、call()で実行する。
-   * やらかすと例外を吐いて無慈悲に死ぬ仕様になっているっぽい。
-   */
-  val originUri: String = "https://github.com/fand/md2hatena"
+  val CLONES_PER_REPO = 4
+  val CLONEPOOL_ROOT = System.getProperty("user.home") + s"/.clonepool/"
 
-  val workingTree: File = new File(System.getProperty("user.home") + s"/.clonepool/md2hatena/")
+  type Repo = String
+  type Uri = String
 
-  val git: Git = getGitInstance()
+  val repo = "fand/md2hatena"
+  var git = getGitInstance(repo)
 
-  def getGitInstance() = {
-    val isInRepository = false
-    if (isInRepository) {
-      new Git(
-        new RepositoryBuilder()
-          .setWorkTree(workingTree)
-          .build()
-      )
+  try {
+    git.fetch().call();
+  } catch {
+    case e: GitAPIException => throw new RuntimeException(e)
+    case _: Throwable => println("fetch failed")
+  }
+
+  // ブランチ名の一覧取得(Ref の getName()で「refs/remotes/origin/master」のように取得できる)
+  val branchList = git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
+
+  for (b <- branchList.toList) {
+    println(b)
+  }
+
+  def getGitInstance(repo: Repo): Git = {
+    if (doesPoolExist(repo) && !shouldClone(repo)) {
+      getGitExisting(repo)
     }
     else {
-      new CloneCommand()
-        .setDirectory(workingTree)
-        .setURI(originUri)
-        .call()
+      getGitClone(repo)
     }
+  }
+
+  def doesPoolExist(repo: Repo): Boolean = {
+    new File(getCloneDst(repo)).exists
+  }
+
+  def shouldClone(repo: Repo): Boolean = {
+    new File(getCloneDst(repo)).list.size < CLONES_PER_REPO
+  }
+
+  def getOriginUri(repo: Repo): Uri = {
+    s"https://github.com/$repo"
+  }
+
+  def getCloneDst(repo: Repo): String = {
+    s"$CLONEPOOL_ROOT/$repo/"
+  }
+
+  def getGitClone(repo: Repo): Git = {
+    new CloneCommand()
+      .setDirectory(new File(getCloneDst(repo)))
+      .setURI(getOriginUri(repo))
+      .call()
+  }
+
+  def getGitExisting(repo: Repo): Git = {
+    new Git(
+      new RepositoryBuilder()
+        .setWorkTree(new File(getCloneDst(repo)))
+        .build()
+    )
   }
 
 }
