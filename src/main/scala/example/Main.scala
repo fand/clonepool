1ee43909aa4abe126f39a31cf2d6831de32d20e3
amagitakayosi <fand.gmork@gmail.com>
2017-03-09 11:32:29 +0900

    Use jgit

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,9 +1,36 @@
 package io.github.fand.clonepool
+import java.io.File
+import org.eclipse.jgit.api._
+import org.eclipse.jgit.lib._
 
-object Main extends Yo with App {
-  println(yo)
-}
+object Main extends App {
 
-trait Yo {
-  def yo = "yo"
+  /**
+   * まずCloneなりローカルなレポジトリを教えるなりして生成したgitのインスタンスを叩くことで各種レポジトリ操作を実行する。
+   * コマンドはメソッドチェインで構築し、call()で実行する。
+   * やらかすと例外を吐いて無慈悲に死ぬ仕様になっているっぽい。
+   */
+  val originUri: String = "https://github.com/fand/md2hatena"
+
+  val workingTree: File = new File(System.getProperty("user.home") + s"/.clonepool/md2hatena/")
+
+  val git: Git = getGitInstance()
+
+  def getGitInstance() = {
+    val isInRepository = false
+    if (isInRepository) {
+      new Git(
+        new RepositoryBuilder()
+          .setWorkTree(workingTree)
+          .build()
+      )
+    }
+    else {
+      new CloneCommand()
+        .setDirectory(workingTree)
+        .setURI(originUri)
+        .call()
+    }
+  }
+
 }
--- src/test/scala/example/MainSpec.scala
+++ src/test/scala/example/MainSpec.scala
@@ -3,7 +3,7 @@ package io.github.fand.clonepool
 import org.scalatest._
 
 class MainSpec extends FlatSpec with Matchers {
-  "The Main object" should "say yo" in {
-    Main.yo shouldEqual "yo"
-  }
+  // "The Main object" should "say yo" in {
+  //   Main.yo shouldEqual "yo"
+  // }
 }
