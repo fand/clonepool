59b8fd2099f2447a48a9df858478d0fdfd4dc1f1
fand <fand.gmork@gmail.com>
2017-03-31 15:16:36 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+VERSION=$1
+if [[ $VERSION == '' ]]; then
+  echo 'Usage:'
+  echo '  $ ./build.sh VERSION'
+  exit 1
+fi
+
+DIR="clonepool-$VERSION"
+TAR="$DIR.tar.gz"
+
+sbt assembly
+mkdir $DIR
+mv target/scala-2.12/Clonepool-assembly-$VERSION.jar $DIR/clonepool.jar
+tar zcvf $TAR $DIR
+rm -f docs/bin/*
+mv $TAR docs/bin
+
+shasum -a 256 docs/bin/$TAR | cut -f1 -d' '
+rm -rf $DIR
