7b51e95ad53b934159981992bdff6b522723c5ff
fand <fand.gmork@gmail.com>
2017-04-06 23:07:16 +0900

    Remove tarball from repository

--- build.sh
+++ build.sh
@@ -13,8 +13,6 @@ sbt assembly
 mkdir $DIR
 mv target/scala-2.11/Clonepool-assembly-$VERSION.jar $DIR/clonepool.jar
 tar zcvf $TAR $DIR
-rm -f docs/bin/*
-mv $TAR docs/bin
 
-shasum -a 256 docs/bin/$TAR | cut -f1 -d' '
+shasum -a 256 $TAR | cut -f1 -d' '
 rm -rf $DIR
