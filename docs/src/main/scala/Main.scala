a7418df6c564fbe61e6bae5d5af21b02b61a00c8
fand <fand.gmork@gmail.com>
2017-04-06 22:27:51 +0900

    Show a hint on console

--- docs/src/main/scala/Main.scala
+++ docs/src/main/scala/Main.scala
@@ -1,11 +1,28 @@
 package io.github.fand.clonepool.docs
 import io.github.fand.clonepool.docs.components.App
 import org.scalajs.dom.document
+import scala.util.Try
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.Dynamic.global
 
 object Main extends JSApp {
+  val userAgent: String =
+    Try(global.navigator.userAgent.asInstanceOf[String]) getOrElse "Unknown"
+  val isChrome  = userAgent contains "Chrome"
+
   override def main(): Unit = {
     Styles.addToDocument()
     App.component().renderIntoDOM(document.querySelector("#app"))
+
+    if (isChrome) {
+      global.console.log(
+        "%c+%c  < Click us!",
+        "font-size: 1px; padding: 26px 23px; line-height: 64px; background: url('https://fand.github.io/clonepool/images/clone.png'); background-size: 51px 64px; color: transparent",
+        ""
+      )
+    }
+    else {
+      global.console.log("Click Octocats!")
+    }
   }
 }
